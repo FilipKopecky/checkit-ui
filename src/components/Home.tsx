4568a0703a5373cadf2fbaeac3f3ef3343a73878
Filip Kopecký <kopeckyfil@gmail.com>
2023-02-08 15:49:34 +0100

    Added styling library

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import {Box, Typography} from "@mui/material";
 
 const Home: React.FC = () => {
   return (
-    <div>
-      <h1>Home page</h1>
-    </div>
+    <Box>
+        <Typography variant={"h1"}>
+            CheckIt
+        </Typography>
+    </Box>
   );
 };
 
