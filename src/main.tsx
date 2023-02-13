20cef7728b07571bd55bc37b960defabfc14f7ad
Filip Kopecký <kopeckyfil@gmail.com>
2023-02-13 22:47:44 +0100

    Removed unnecessary logging

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client";
 import Router from "./components/Router";
 import { Auth } from "./components/auth/components";
 
-console.log(import.meta.env);
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Auth>
