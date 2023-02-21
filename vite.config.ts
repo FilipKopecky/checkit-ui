43c38ea9815e18f01cf4ed6db75112041670b52e
Filip Kopecký <kopeckyfil@gmail.com>
2023-02-21 16:08:01 +0100

    Keeping the AL naming convention

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-export default ({ mode }) => {
-  // Load app-level env vars to node-level env vars.
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
-  return defineConfig({
-    base: "/modelujeme/sluzby/v-nástroji/checkit",
-    plugins: [react()],
-  });
-};
+export default defineConfig({
+  base: "/modelujeme/v-nástroji/checkit",
+  plugins: [react()],
+});
