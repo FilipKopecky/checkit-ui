ac4c854673d266bc9e8d3803f5b5058f0812e782
Filip Kopecký <kopeckyfil@gmail.com>
2023-02-23 15:07:23 +0100

    Added linter

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import eslint from 'vite-plugin-eslint';
 
 export default defineConfig({
   base: "/modelujeme/v-nástroji/checkit",
-  plugins: [react()],
+  plugins: [react(), eslint()],
 });
