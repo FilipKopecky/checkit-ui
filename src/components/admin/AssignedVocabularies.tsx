6c045a36ea6c8b3be83a3df146f5dcbd49614646
Filip Kopecký <kopeckyfil@gmail.com>
2023-03-06 23:04:08 +0100

    Reformatted assigned vocabularies

--- src/components/admin/AssignedVocabularies.tsx
+++ src/components/admin/AssignedVocabularies.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import AllVocabularies from "../vocabulary/AllVocabularies";
+import { Box, Typography } from "@mui/material";
+import { useIntl } from "react-intl";
 
 const AssignedVocabularies: React.FC = () => {
-  return <AllVocabularies />;
+  const intl = useIntl();
+  return (
+    <Box px={3}>
+      <Typography variant={"h4"}>
+        {intl.formatMessage({ id: "assignedVocabulariesHeader" })}
+      </Typography>
+      <Box py={2}>
+        <AllVocabularies />
+      </Box>
+    </Box>
+  );
 };
 
 export default AssignedVocabularies;
