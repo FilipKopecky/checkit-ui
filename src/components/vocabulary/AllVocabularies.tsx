9e579bc19a6bd4cd5d855b5d0e919d26526ba900
Filip Kopecký <kopeckyfil@gmail.com>
2023-03-08 01:11:33 +0100

    Moved memo outside the parent component

--- src/components/vocabulary/AllVocabularies.tsx
+++ src/components/vocabulary/AllVocabularies.tsx
@@ -6,35 +6,35 @@ import MuiList from "@mui/material/List";
 import ListItemText from "@mui/material/ListItemText";
 import IconButton from "@mui/material/IconButton";
 import PersonAddAltIcon from "@mui/icons-material/PersonAddAlt";
+
+const InnerItem = React.memo(({ index, vocabularyLabel }: any) => {
+  return (
+    <>
+      <ListItem
+        sx={{
+          backgroundColor:
+            index % 2 === 0 ? "background.default" : "background.paper",
+        }}
+        secondaryAction={
+          <IconButton
+            edge="end"
+            aria-label="delete"
+            onClick={() => console.log(`Clicked on index: ${index}`)}
+          >
+            <PersonAddAltIcon />
+          </IconButton>
+        }
+      >
+        <ListItemText primary={vocabularyLabel} />
+      </ListItem>
+    </>
+  );
+});
 const AllVocabularies: React.FC = () => {
   const { data, isLoading } = useGetAllVocabulariesQuery();
 
   if (isLoading) return <>Loading...</>;
   if (!data) return <>Fail</>;
-
-  const InnerItem = React.memo(({ index, vocabularyLabel }: any) => {
-    return (
-      <>
-        <ListItem
-          sx={{
-            backgroundColor:
-              index % 2 === 0 ? "background.default" : "background.paper",
-          }}
-          secondaryAction={
-            <IconButton
-              edge="end"
-              aria-label="delete"
-              onClick={() => console.log(`Clicked on index: ${index}`)}
-            >
-              <PersonAddAltIcon />
-            </IconButton>
-          }
-        >
-          <ListItemText primary={vocabularyLabel} />
-        </ListItem>
-      </>
-    );
-  });
 
   const itemContent = (index: any, vocabulary: any) => {
     return <InnerItem index={index} vocabularyLabel={vocabulary.label} />;
