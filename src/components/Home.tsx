172b6d181033dded542efbe704468046e5874fbf
Filip Kopecký <kopeckyfil@gmail.com>
2023-03-16 15:07:54 +0100

    Home navigation cards

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import { Typography } from "@mui/material";
 import Box from "@mui/material/Box";
 import { useIntl } from "react-intl";
+import HomeNavigationButtons from "./HomeNavigationButtons";
 
 const Home: React.FC = () => {
   const intl = useIntl();
   return (
-    <Box>
-      <Typography variant={"h1"}>
+    <Box p={3}>
+      <Typography variant={"h4"} gutterBottom>
         {intl.formatMessage({ id: "welcome" })}
       </Typography>
+      <HomeNavigationButtons />
     </Box>
   );
 };
